c50b1dcf0c9f20b08e8faf8e818f735f4cbb5a64
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2016-06-07 15:00:07 +0200

    Add gidem alias
    Usefull when you do a series of quasi identical commits
    
    gidem do:
      - git add --all
      - git commit -c HEAD (to reuse the commit information of the last commit you did and go to edit mode)

--- git-xavhan.plugin.zsh
+++ git-xavhan.plugin.zsh
@@ -15,6 +15,7 @@ alias gpub='git push --set-upstream origin $(current_branch)'
 alias gunpub='git branch --unset-upstream'
 alias gcpa='git commit --patch'
 alias goops='gcan!'
+alias gidem='gca -c HEAD'
 alias gpr='git pull-request | pbcopy'
 alias glast="gd --name-only | xargs atom-beta"
 alias gres='git diff --name-only --diff-filter=U | uniq  | xargs atom-beta'
