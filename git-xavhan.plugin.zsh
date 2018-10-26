5e07d264929c055c7075cd7e8e03e155f2eb6b25
Xavier Haniquaut <xavhan@hotmail.com>
2018-10-26 11:56:07 +0200

    Add some documentation

--- git-xavhan.plugin.zsh
+++ git-xavhan.plugin.zsh
@@ -1,31 +1,54 @@
-function current_branch() {
-  local ref
-  ref=$($_omz_git_git_cmd symbolic-ref --quiet HEAD 2> /dev/null)
-  local ret=$?
-  if [[ $ret != 0 ]]; then
-    [[ $ret == 128 ]] && return  # no git repo.
-    ref=$($_omz_git_git_cmd rev-parse --short HEAD 2> /dev/null) || return
-  fi
-  echo ${ref#refs/heads/}
-}
-#
+## git Aliases
+
+# better git branch
 alias gb="git branch -vv"
+
+# force delete branch
 alias gbD='git branch -D'
-alias gpub='git push --set-upstream origin $(current_branch)'
+
+# publish branch (did not like the default alias)
+alias gpub='gpsup'
+
+# unpublish branch
 alias gunpub='git branch --unset-upstream'
+
+# patch commit
 alias gcpa='git commit --patch'
-alias gidem='gca -c HEAD'
-alias gpf='git push --force-with-lease'
-alias gpr='git pull-request'
-alias gprco='git pr checkout'
-alias gcopr='git pr checkout'
+
+# open staging files in code
 alias glast="gd --name-only | xargs code"
-alias gres='git diff --name-only --diff-filter=U | uniq  | xargs code'
+
+# open merge conflicts in vscode
+alias gres='git diff --name-only --diff-filter=U | uniq | xargs code'
+
+# patch reset
 alias grpa='git reset --patch'
+
+# other alias for gsh
 alias gs='git show'
 
+## hub aliases
+
+# PR against master 
+alias gpr='git pull-request'
+
+# checkout PR
+alias gprco='git pr checkout'
+alias gcopr='gprco'
+
+## ghi aliases
+
+# add "Need Review" label
 alias gprnr='f() { ghi label $1 -a "Need Review" };f'
+
+# add "Need Fixes" label
 alias gprnf='f() { ghi label $1 -a "Need Fixes" };f'
+
+# add "Currently in QA" label
 alias gprqa='f() { ghi label $1 -a "Currently in QA" };f'
+
+# remove "Need Review" & "Need Fixes" labels
 alias gprok='f() { ghi label $1 -d "Need Review" -d "Need Fixes" };f'
+
+# remove "Need Review" & "Need Fixes" labels and add "Currently in QA" label
 alias gprpp='f() { gprok $1 && gprqa $1 };f'
